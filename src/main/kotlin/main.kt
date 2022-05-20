d770ba721044c687713c446a57c3d40ac854a647
tqi_afernandes <tqi_afernandes@uolinc.com>
2022-05-20 15:07:21 -0300

    first

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,13 @@
+
+
+fun main() {
+
+    println("bem vindo krasnodar");
+
+    var variavelModifical = "modificavel"
+    val variavelNaoModificavel = "constante"
+
+    //Exemplo de concatenação
+    println("exemplo $variavelModifical")
+
+}
